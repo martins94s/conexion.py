9691d9ac10aa7d4950deb3d746fdb8c4c96fa2de
martins94s <116390953+martins94s@users.noreply.github.com>
2023-05-24 04:49:41 -0300

    Delete mysqlEvidencia3 directory

--- mysqlEvidencia3/big_bread.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE big_bread;
-USE big_bread;
-CREATE TABLE PRODUCCION(fecha int, num_pedido int, producto varchar(30), cantidad int);
-CREATE TABLE PRODUCTO(codigo int, producto varchar(30), receta varchar(30));
-CREATE TABLE RECETA(nom_receta varchar(30), insumo varchar(30), cantidad int);
-CREATE TABLE INSUMO(tipo_insumo varchar(30), stock int);
